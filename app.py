f4833841db02bf3172c642f3a606776de50a853b
baloon-creator <septiaharbie@gmail.com>
2026-04-02 14:02:47 +0700

    First Github Project

---
+++ app.py
@@ -0,0 +1,8 @@
+#Hitung Luas Segitiga sederhana
+def luas_segitiga():
+    a = int(input("Masukkan Alas Segitiga: "))
+    t = int(input("Masukkan Tinggi Segitiga: "))
+    luas = a * t / 2
+    print("Luas Segitiga adalah ", luas)
+
+luas_segitiga()
